47ec55be3bc3208e2d17b29bee9de93df26a6649
R3nTF <148765115+R3nTF@users.noreply.github.com>
2024-11-04 20:03:26 +0000

    Astar part2

--- Assets/Graphs/Node.cs
+++ Assets/Graphs/Node.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+public class Node
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Edge> edgeList = new List<Edge>();
+    public Node path = null;
+    GameObject id;
+    public float xPos;
+    public float yPos;
+    public float zPos;
+
+    public float f, g, h;
+    public Node cameFrom;
+
+    public Node(GameObject i)
     {
-        
+        id = i;
+        xPos = i.transform.position.x;
+        yPos = i.transform.position.y;
+        zPos = i.transform.position.z;
+        path = null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject getId()
     {
-        
+        return id;
     }
 }
